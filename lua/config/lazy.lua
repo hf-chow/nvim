0d9a4bfb16609c07e62f7b5a3284b8cbc6c08714
hf-chow <honfungchow@gmail.com>
2025-06-07 11:05:40 +0800

    updated blink.cmp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -95,7 +95,7 @@ require("lazy").setup({
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
 			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "[P]Search [F]iles" })
 			vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
 			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
@@ -336,6 +336,7 @@ require("lazy").setup({
 		opts = {
 			keymap = {
 				preset = "default",
+				["<enter>"] = { "accept" },
 			},
 
 			appearance = {
@@ -344,6 +345,9 @@ require("lazy").setup({
 
 			completion = {
 				documentation = { auto_show = false, auto_show_delay_ms = 500 },
+				list = {
+					selection = { preselect = true, auto_insert = false },
+				},
 			},
 
 			sources = {
